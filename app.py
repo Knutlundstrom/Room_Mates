c7c87c6f64ea0e18e03ddce8e46182981fba008e
Knutlundstrom <knutsvahnqvist@gmail.com>
2024-11-11 15:06:53 -0500

    Added email validation and updated cleanliness question

--- app.py
+++ app.py
@@ -237,28 +237,28 @@ class UserProfile:
 
 import streamlit as st
 
-def get_user_profile():
-    st.title("Room_Mates/The Future of Roommate Matching")
-    
-    # Subtitle for Alpha test notice
-    st.write("### This is an Alpha test")
-    st.write("Feedback to kl608260@wne.edu very much appreciated.")
-    st.write("If no matches are found due to a small number of users, try again in a few days.")
-    
-    # Collect user inputs
-    name = st.text_input("What is your name?")
-    email_or_instagram = st.text_input("Please type email to be reached out to when matched:")
+# Collect user inputs
+name = st.text_input("What is your name?")
+email_or_instagram = st.text_input("Please type email to be reached out to when matched:")
 
-    # Collect cleanliness preference
-    cleanliness = st.selectbox(
-        "How clean are you?", 
-        options=[1, 2, 3], 
-        format_func=lambda x: {
-            1: "1: I have an assigned day weekly for deep cleaning, dishes are never in the sink, and my cleaning routine is set",
-            2: "2: I take care of my room that's it and leave dishes when I am tired",
-            3: "3: I clean when I feel like it or seems to be time"
-        }[x]
-    )
+# Validation for email input
+email_valid = bool(re.match(r"^[a-zA-Z0-9._%+-]+@(gmail\.com|.+\.edu)$", email_or_instagram))
+
+# Show a warning if email is not valid
+if email_or_instagram and not email_valid:
+    st.warning("Please enter a valid email address ending with .edu or @gmail.com.")
+
+# Collect cleanliness preference
+cleanliness = st.selectbox(
+    "How clean are you?", 
+    options=[1, 2, 3], 
+    format_func=lambda x: {
+        1: "1: I have an assigned day weekly for deep cleaning, dishes are never in the sink, and my cleaning routine is set",
+        2: "2: I take care of my room that's it and leave dishes when I am tired",
+        3: "3: I clean when I feel like it or seems to be time"
+    }[x]
+)
+
 
     age = st.number_input("Enter your age:", min_value=0, max_value=120)
 
